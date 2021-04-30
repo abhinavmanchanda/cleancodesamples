d136b45b48d495e53e913dba64c0abb46c621fc8
Abhinav Manchanda <abhinav@abhinav.local>
2021-04-30 05:57:06 +0530

    Fixed issues in initial code

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import math
 def main():
     n = int(input())
     o=0
-    for i in range(2, math.floor(n/2+1)):
+    for i in range(2, int(n/2+1)):
         f = 1
-        for j in range(2, math.floor(i/2+1)):
+        for j in range(2, int(i/2+1)):
             if (i%j==0):
                 f=0
-        for j in range(2, math.floor((n-i)/2+1)):
+        for j in range(2, int((n-i)/2+1)):
             if((n-i)%j==0):
                 f=0
         if(f==1):
